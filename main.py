727d7b92af553c7fb1d7082533b9f7c2440583e8
Josh Dudas <josh.dudas@dish.com>
2019-12-16 11:20:26 -0700

    Added try/except to see if links from Popular Searches are working

--- main.py
+++ main.py
@@ -8,20 +8,27 @@ class TestAutomatedChromeBrowser(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome('./chromedriver')
 
-    def test_getMakeModelList_Urls(self):
+    def test_get_Popular_Searches_Items_and_Urls(self):
         driverwait = WebDriverWait(self.driver, 10)
         self.driver.get('https://www.copart.com')
         driverwait.until(expected_conditions.visibility_of_element_located((By.XPATH, '//div[@id="tabTrending"]')))
         popular_items_list = self.driver.find_elements(By.XPATH, '//*[@id="tabTrending"]//a')
 
         popular_items_matrix = [[],[]]
-        for item in popular_items_matrix:
+        for item in popular_items_list:
             popular_items_matrix[0].append(item.text)
             popular_items_matrix[1].append(item.get_attribute('href'))
 
         i = 0
         while i < len(popular_items_matrix[0]):
-            print(popular_items_matrix[0][i] + " - " + popular_items_matrix[1][i])
+            try:
+                self.driver.get(popular_items_matrix[1][i])
+                print("{}'s link {} is working".format(str(popular_items_matrix[0][i]).upper(),
+                                                               str(popular_items_matrix[1][i]).upper()))
+                i += 1
+            except TimeoutError as te_error:
+                print("ERROR: {}'s link ({}) timed out".format(str(popular_items_matrix[0][i]).upper(), str(popular_items_matrix[1][i]).upper()))
+                print(te_error)
             i += 1
 
     def tearDown(self):
